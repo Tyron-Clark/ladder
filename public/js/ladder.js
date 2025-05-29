156d337e33e0d5dbe19e7e7cac0cb8c6d13a46bc
tyrontyc <tyron.c0305@gmail.com>
2025-05-29 04:15:12 -0400

    ladder state

--- public/js/ladder.js
+++ public/js/ladder.js
@@ -1,13 +1,45 @@
-const fetchLeaderboardData = (season = 11, bracket = "2v2", region = "us") => {
+$(document).ready(function () {
+  fetchLeaderboardData();
+});
+
+///// Ladder Data /////
+
+let ladderState = {
+  season: 11,
+  bracket: "2v2",
+  region: "us",
+};
+
+const fetchLeaderboardData = (options = {}) => {
+  ladderState = { ...ladderState, ...options };
+
   $.get("/api/leaderboard", {
-    season: season,
-    bracket: bracket,
-    region: region,
+    season: ladderState.season,
+    bracket: ladderState.bracket,
+    region: ladderState.region,
   })
     .done(function (data) {
-      console.log("leaderboard data:", data);
+      console.log(
+        `Loaded ${
+          ladderState.bracket
+        } leaderboard for ${ladderState.region.toUpperCase()} server:`,
+        data
+      );
     })
     .fail(function (xhr, status, error) {
-      console.error("failed to fetch leaderboard:", error);
+      console.error("Failed to fetch leaderboard:", error);
     });
 };
+
+///// Ladder filtering buttons /////
+
+$("[data-leaderboard-param]").click(function () {
+  const button = $(this);
+  const paramName = button.data("leaderboard-param");
+  const paramValue = button.data("value");
+
+  const options = {};
+  options[paramName] = paramValue;
+
+  fetchLeaderboardData(options);
+});
